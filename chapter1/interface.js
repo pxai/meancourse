33c85dd383333dd4f6b7dd681e8c58f549c27aa2
pello_altadill <pello_altadill@cuatrovientos.org>
2015-11-18 20:24:11 +0100

    Firts commit

---
+++ chapter1/interface.js
@@ -0,0 +1,21 @@
+var db = require('./connect');
+/*
+ *  Inserts "doc" into the collection "movies".
+ */
+exports.insert = function(db, doc, callback) {
+  db.collection('movies').insert(doc, callback);
+  // TODO: implement
+  callback(null);
+};
+
+/*
+ *  Finds all documents in the "movies" collection
+ *  whose "director" field equals the given director,
+ *  ordered by the movie's "title" field. See
+ *  http://mongodb.github.io/node-mongodb-native/2.0/api/Cursor.html#sort
+ */
+exports.byDirector = function(db, director, callback) { 
+  db.collection('movies').find({"director":director}).toArray(callback);
+  // TODO: implement
+  callback(null, []);
+};
